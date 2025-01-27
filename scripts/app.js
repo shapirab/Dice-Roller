6e6f9c34de17ff54ebbafe8410dbf6edaa05f9c6
shapirab <shapirab@jamd.ac.il>
2025-01-27 10:20:27 +0200

    27/1/25 completed game functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,41 @@
-let diceConfigs = document.querySelectorAll('.dots--container');
-console.log(diceConfigs)
+let player1 = document.getElementById('player_1');
+let player2 = document.getElementById('player_2');
+let player1Cube = player1.querySelectorAll('.dots--container');
+let player2Cube = player2.querySelectorAll('.dots--container');
+let rollBtn = document.querySelector('.btn');
+let winnerName = document.getElementById('winner-name');
+let actionResults = document.querySelector('.action-results');
 
+rollBtn.addEventListener('click', rollDice);
 
-
-function displayDice(diceConfigId){
-
+function rollDice(){
+    let diceValuePlayer1 = rollOneDice(player1Cube) + 1;
+    let diceValuePlayer2 = rollOneDice(player2Cube) + 1;
+    
+    showWinner(diceValuePlayer1, diceValuePlayer2);
 }
+
+function rollOneDice(playerCube){
+    let ramdomDiceValue = getRandomDiceValue();
+    playerCube.forEach((element, index) => {
+        element.classList.remove('active');
+        if(index === ramdomDiceValue){
+            element.classList.add('active');
+        }
+    });
+    return ramdomDiceValue;
+}
+
+function getRandomDiceValue(){
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+function showWinner(diceValuePlayer1, diceValuePlayer2){
+    let winnerNameText = diceValuePlayer1 > diceValuePlayer2 ? 'Player 1 wins!':'Player 2 wins!';
+    if(diceValuePlayer1 === diceValuePlayer2){
+        winnerNameText = 'draw';
+    }
+    winnerName.innerText = winnerNameText;
+    actionResults.classList.add('active');
+}
+
